748ea3605c8e59b935838fb6a89a1ae704474622
Lawrence Otieno <lawrence.otieno@student.moringaschool.com>
2021-07-20 14:57:10 +0300

    js redo

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function leapYear(year){
     if (year%400===0) alert('The year: ' +year+ ' is leap year');
     else if (year%100===0) alert('The year: ' +year+ ' is NOT leap year');
     else if (year%4===0) alert('The year: ' +year+ ' is leap year')
-    else alert('Please enter correct year')
+    else alert('The year: ' +year+ ' is NOT leap year')
   }
   leapYear(parseInt(prompt('Enter year')))
